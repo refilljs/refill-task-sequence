4bf83970b64cd827005a333d9ffaac9c5da76250
Piotr Baranowski <piotr.baranowski@zaklinaczekodu.com>
2016-10-02 00:03:05 +0200

    changed lodash.assign to Object.assign

--- src/sequenceTask.js
+++ src/sequenceTask.js
@@ -1,13 +1,11 @@
 'use strict';
 
-var assign = require('lodash.assign');
-
 function getSequenceTask(options, gulp, mode) {
 
   var runSequence = require('run-sequence').use(gulp);
 
   function sequenceTask(next) {
-    assign(mode, options.mode);
+    Object.assign(mode, options.mode);
     runSequence.apply(null, options.sequence.concat([next]));
   }
 
